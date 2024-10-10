322677ff159b404fc6c4dcd15103b6c1ba561eb1
Javier-Gondres <jegd0001@ce.pucmm.edu.do>
2024-10-10 16:32:51 -0400

    Mas comentarios

--- src/PrintQueue.java
+++ src/PrintQueue.java
@@ -4,15 +4,24 @@ import java.util.Queue;
 public class PrintQueue {
     Queue<PrintJob> printJobs = new LinkedList<>();
 
+    /*
+     * Inserta un elemento al principio de la cola
+     * */
     public void enqueue(PrintJob job){
         printJobs.offer(job);
     }
 
+    /*
+     * Devuelve la impresion que se encuentre al frente
+     * */
     public PrintJob dequeue(){
         PrintJob print = printJobs.poll();
         return print;
     }
 
+    /*
+     * Muestra todas las impresiones
+     * */
     public void showQueue(){
         if(printJobs.isEmpty()){
             System.out.println("Ya no hay mas impresiones en espera.");
